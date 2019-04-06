4920b7844613dcc0fe8c4252eb09957ab35818a4
osojoch <jossarcavi@hotmail.com>
2019-04-05 21:37:44 -0600

    arreglar server 2.0

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 
 var express = require('express');
 var app = express();
-var port=3000;
+var port= process.env.PORT || 9000;
 
 app.get('/', function (req, res) {
   res.send('Hola perro!');
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ${port}!');
 });
